38f96deadf176844e3e385f0c2158116d4bc4eb3
Jorge Aguilera <jagedn@gmail.com>
2015-11-13 14:37:13 +0100

    initial commit

---
+++ carsharing/carsharing-business/src/main/java/edu/uoc/mije/carsharing/business/App.java
@@ -0,0 +1,15 @@
+package edu.uoc.mije.carsharing.business;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ carsharing/carsharing-jsf/src/main/java/edu/uoc/mije/carsharing/jsf/AppBean.java
@@ -0,0 +1,13 @@
+package edu.uoc.mije.carsharing.jsf;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppBean 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
