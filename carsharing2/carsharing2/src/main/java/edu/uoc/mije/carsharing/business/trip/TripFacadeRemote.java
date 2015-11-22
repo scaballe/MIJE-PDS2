ffa3cd6e10d845452a60d98e60e2a63fc29fea6a
emiliew <emiliomolinaharo@gmail.com>
2015-11-22 23:55:11 +0100

    trip beans

--- carsharing2/carsharing2/src/main/java/edu/uoc/mije/carsharing/business/trip/TripFacadeRemote.java
+++
@@ -1,5 +0,0 @@
-package edu.uoc.mije.carsharing.business.trip;
-
-public interface TripFacadeRemote {
-
-}
--- carsharing2/carsharing2/src/main/java/edu/uoc/mije/carsharing/integration/TripJPA.java
+++ carsharing2/carsharing2/src/main/java/edu/uoc/mije/carsharing/integration/TripJPA.java
@@ -1,5 +1,28 @@
 package edu.uoc.mije.carsharing.integration;
 
-public class TripJPA {
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@Entity
+
+public class TripJPA implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public TripJPA() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	Integer id;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 
 }
