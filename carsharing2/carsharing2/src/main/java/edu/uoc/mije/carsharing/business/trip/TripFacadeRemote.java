6500fa258e4223921d0f1e7b907bae72bcf9f659
emiliew <emiliomolinaharo@gmail.com>
2015-11-24 01:22:13 +0100

    facaderemote complete

--- carsharing2/carsharing2/src/main/java/edu/uoc/mije/carsharing/business/trip/TripFacadeRemote.java
+++ carsharing2/carsharing2/src/main/java/edu/uoc/mije/carsharing/business/trip/TripFacadeRemote.java
@@ -1,5 +1,19 @@
 package edu.uoc.mije.carsharing.business.trip;
 
+import java.util.Collection;
+import java.util.Date;
+
+import javax.ejb.Remote;
+
+import edu.uoc.mije.carsharing.integration.TripJPA;
+
+@Remote
 public interface TripFacadeRemote {
+
+	public Collection<TripJPA> findTrip(String departureCity, String arrivalCity, 
+			float minPrice, float maxPrice, Date departureDate);
+	public TripJPA showTrip(int tripId);
+	public boolean RegisterInTrip(int tripId, String passenger);
+	public boolean removeFromTrip(int tripId, String passenger);
 
 }
