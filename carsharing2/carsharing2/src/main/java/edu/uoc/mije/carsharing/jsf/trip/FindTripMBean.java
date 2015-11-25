f155c1584c8c98b93e099073628e9a29b2dbb4d2
emiliew <emiliomolinaharo@gmail.com>
2015-11-25 02:06:55 +0100

    started to implement FindTripMBean

--- carsharing2/carsharing2/src/main/java/edu/uoc/mije/carsharing/jsf/trip/FindTripMBean.java
+++ carsharing2/carsharing2/src/main/java/edu/uoc/mije/carsharing/jsf/trip/FindTripMBean.java
@@ -1,5 +1,38 @@
 package edu.uoc.mije.carsharing.jsf.trip;
 
+import java.util.Collection;
+import java.util.Date;
+
+import javax.ejb.EJB;
+
+import edu.uoc.mije.carsharing.business.trip.TripFacadeRemote;
+import edu.uoc.mije.carsharing.integration.TripJPA;
+
 public class FindTripMBean {
+
+	@EJB
+	private TripFacadeRemote tripRemote;
+	
+	private String departureCity;
+	private String arrivalCity;
+	private float minPrice;
+	private float maxPrice;
+	private Date departureDate;
+	
+	//stores all the instances of TripJPA
+	private Collection<TripJPA> tripList;
+	
+	/**
+	 * Constructor method
+	 * @throws Exception
+	 */
+	public FindTripMBean(String departureCity, String arrivalCity, float minPrice, float maxPrice, Date departureDate) throws Exception{
+		this.departureCity = departureCity;
+		this.arrivalCity = arrivalCity;
+		this.minPrice = minPrice;
+		this.maxPrice = maxPrice;
+		this.departureDate = departureDate;
+		//this.tripList();
+	}
 
 }
