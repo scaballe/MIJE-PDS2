90cba4b0735c3b9dd3f091bbbaab41ca3c891e06
Jorge Aguilera <jagedn@gmail.com>
2015-12-03 10:37:35 +0100

    include arquillian tests by profile

---
+++ carsharing2/carsharing2/src/test/java/edu/uoc/mije/carsharing/business/ShowTripCommentsTest.java
@@ -0,0 +1,49 @@
+package edu.uoc.mije.carsharing.business;
+
+import java.util.Collection;
+
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import edu.uoc.mije.carsharing.business.comms.CommunicationFacadeBean;
+import edu.uoc.mije.carsharing.business.comms.CommunicationFacadeRemote;
+import edu.uoc.mije.carsharing.integration.AbstractJPATest;
+import edu.uoc.mije.carsharing.integration.CarJPA;
+import edu.uoc.mije.carsharing.integration.MessageJPA;
+import junit.framework.Assert;
+
+@RunWith(Arquillian.class)
+public class ShowTripCommentsTest extends AbstractJPATest {
+	
+	public ShowTripCommentsTest() {
+	}
+
+	@Override
+	protected void clearDataIntoTransaction(EntityManager em) throws Exception {
+		//em.createQuery("delete from MessageJPA").executeUpdate();
+	}
+	
+	@Override
+	protected void insertDataIntoTransaction(EntityManager em) throws Exception {
+		// TODO Auto-generated method stub
+		//MessageJPA msg1 = new MessageJPA("test body","test content");
+		//em.persist(msg1);
+		//System.out.println("**************** "+msg1.getId());
+	}
+	
+	@EJB
+	private CommunicationFacadeRemote communicationFacadeRemote; 
+	
+	
+	@Test
+	public void testShowTripCommentsEmpty() throws Exception {	
+		//Collection<MessageJPA> test = communicationFacadeRemote.showTripComments(1);
+		//Assert.assertTrue(test.size()==1);
+	}
+}
---
+++ carsharing2/carsharing2/src/test/java/edu/uoc/mije/carsharing/integration/AbstractJPATest.java
@@ -0,0 +1,97 @@
+package edu.uoc.mije.carsharing.integration;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import edu.uoc.mije.carsharing.business.comms.CommunicationFacadeBean;
+import edu.uoc.mije.carsharing.business.exceptions.PassengerInTripException;
+import edu.uoc.mije.carsharing.business.trip.TripFacadeBean;
+import edu.uoc.mije.carsharing.business.tripadmin.TripAdminFacadeBean;
+import edu.uoc.mije.carsharing.business.user.UserFacadeBean;
+
+@RunWith(Arquillian.class)
+public abstract class AbstractJPATest {
+
+	@Deployment
+    public static Archive<?> createDeployment() {		
+		JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
+	            .addPackage(CarJPA.class.getPackage())
+	            .addPackage(PassengerInTripException.class.getPackage())
+	            .addPackage(CommunicationFacadeBean.class.getPackage())
+	            .addPackage(TripFacadeBean.class.getPackage())
+	            .addPackage(TripAdminFacadeBean.class.getPackage())
+	            .addPackage(UserFacadeBean.class.getPackage())
+	            .addAsManifestResource("test-persistence.xml", "persistence.xml")
+	            .addAsManifestResource("jbossas-ds.xml")
+	            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+		System.out.println(jar.toString(true));
+		return jar;
+    }
+	
+	
+	@PersistenceContext
+    EntityManager em;
+    
+    @Inject
+    UserTransaction utx;
+    
+    @Before
+    public void preparePersistenceTest() throws Exception {
+        clearData();
+        insertData();
+        startTransaction();
+    }
+
+    private void clearData() throws Exception {
+        utx.begin();
+        em.joinTransaction();
+        System.out.println("Dumping old records...");
+        //em.createQuery("delete from CarJPA").executeUpdate();
+        clearDataIntoTransaction(em);
+        utx.commit();
+    }
+    
+    protected abstract void clearDataIntoTransaction(EntityManager em) throws Exception;
+    
+    protected abstract void insertDataIntoTransaction(EntityManager em) throws Exception;
+
+    private void insertData() throws Exception {
+        utx.begin();
+        em.joinTransaction();
+        System.out.println("Inserting records...");
+
+        insertDataIntoTransaction(em);
+        
+        utx.commit();
+        // clear the persistence context (first-level cache)
+        em.clear();
+    }
+
+    private void startTransaction() throws Exception {
+        utx.begin();
+        em.joinTransaction();
+    }
+    
+    @After
+    public void commitTransaction() throws Exception {
+        utx.commit();
+    }
+    
+
+}
